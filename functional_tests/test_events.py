56866fe1ba9ad10769f4637474a34315397b219f
Jakub Nurski <nurskijakub@gmail.com>
2016-11-10 21:47:50 +0100

    Cleaned templates by dividing base into blocks. Changed FT a bit.

--- functional_tests/test_events.py
+++ functional_tests/test_events.py
@@ -1,14 +1,13 @@
 from .base import FunctionalTest
 
 PAGE_TITLE = '.NET events'
-NEW_SCHEDULE_BUTTON_TEXT = 'Create new schedule'
 
 
 class NewVisitorTest(FunctionalTest):
 
-    def test_can_start_new_event(self):
-        # Jacob got message from .NET leader to try out new page project
-        # He goes to check out its homepage on localhost
+    def test_can_see_all_home_page_elements(self):
+        # Jacob got message from .NET leader to try out new page project.
+        # He goes to check out its homepage on localhost.
         print(self.server_url)
         self.browser.get(self.server_url)
 
@@ -17,7 +16,7 @@ class NewVisitorTest(FunctionalTest):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn(PAGE_TITLE, header_text)
 
-        # There is also a large button prompting to start new schedule
-        new_schedule_button_text = self.browser.find_element_by_id(
-                                        'new-schedule-button').text
-        self.assertIn(NEW_SCHEDULE_BUTTON_TEXT, new_schedule_button_text)
+        # There is also a button for logging in.
+
+        # And the largest from them all: schedule showing all upcoming events
+        # for one of the .NET groups.
