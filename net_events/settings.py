8e0c91897d51114c2975e92a08d5d9bce3642749
Jakub Nurski <nurskijakub@gmail.com>
2016-11-11 00:05:29 +0100

    Tried SASS compress, doesn't work because of MSVC++ compiler and bad encoding. MOVING TO LINUX !

--- net_events/settings.py
+++ net_events/settings.py
@@ -15,6 +15,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party apps
+    'sass_processor',
+
     # Custom apps
     'functional_tests',
     'events',
@@ -99,6 +103,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
-print(STATIC_URL)
-print(STATIC_ROOT)
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
