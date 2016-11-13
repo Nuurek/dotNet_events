e415ed8766be0a7fbfd8835bebac533755ec0066
Kuba <nurskijakub@gmail.com>
2016-11-13 21:26:16 +0100

    Moved base.html template to common templates folder.

--- net_events/settings.py
+++ net_events/settings.py
@@ -39,7 +39,9 @@ ROOT_URLCONF = 'net_events.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
